1fbcea955aaab74bbe311e0decc1f9bf772e042d
Dheeraj Karki <dheerajkarki1790@outlook.com>
2018-08-03 01:50:09 +0530

    Creating Initial WebPack Configuration

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    context: __dirname + "/public",
+    entry: "./app.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: './js/app.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CopyWebpackPlugin([
+            { from: './css/', to: '../dist/css/' },
+            { from: './assets/media/', to: '../dist/assets/media/' }
+        ]),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: /src/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist/assets/media'),
+        compress: true,
+        port: 12000,
+        stats: 'errors-only',
+        open: true,
+        inline: true
+    },
+    devtool: 'inline-source-map',
+};
